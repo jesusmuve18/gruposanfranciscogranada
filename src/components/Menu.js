2b826e368e6197321b4a1b796fe69b1f748eda47
victoria.granados <victoria.granados@vcomm.es>
2025-11-26 07:45:52 +0100

    Cambio en el estilo del menú de la página principal
    Cambio de la tipografía del cuerpo
    
    Se añade el submenú de ministerios

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../styles/Menu.css'
 import { Logo } from "./Logo";
 import { MenuItem } from "./MenuItem"
@@ -8,9 +8,23 @@ import FadeItem from "./FadeItem";
 import imagenApostoles from '../img/apostoles.jpg'
 
 export const Menu = (props) => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 20);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    const isTall = props.selected === "pagina-principal" && !isScrolled;
+
     return (
         <div className="Menu">
-            <img src={imagenApostoles} alt="imagenApostoles" className={`menu-img ${props.selected === "pagina-principal" ? "tall" : "short"}`}/>
+            <img src={imagenApostoles} alt="imagenApostoles" className={`menu-img ${isTall ? "tall" : "short"}`}/>
 
             <div className="overlay"></div>
 
@@ -20,11 +34,15 @@ export const Menu = (props) => {
                     <div className="menu-items">
                         <MenuItem name={"Inicio"} link={"pagina-principal"} setSelected={props.setSelected} selected={props.selected}/>
                         <MenuItem name={"La casa"} link={"la-casa"} setSelected={props.setSelected} selected={props.selected}/>
-                        <MenuItem name={"Ministerios"} link={"ministerios"} setSelected={props.setSelected} selected={props.selected}/>
+                        <MenuItem name={"Ministerios"} link={"ministerios"} setSelected={props.setSelected} selected={props.selected} subItems={[
+                            { name: "Oración", link: "ministerio-oracion" },
+                            { name: "Música", link: "ministerio-musica" },
+                            { name: "Liturgia", link: "ministerio-liturgia" }
+                        ]}/>
                         <MenuItem name={"Contacto"} link={"contacto"} setSelected={props.setSelected} selected={props.selected}/>
                     </div>
                 </div>
-                {(props.selected === "pagina-principal") &&
+                {(props.selected === "pagina-principal" && !isScrolled) &&
                     <FadeItem className="mensaje">
                         <div className="cita">"Venid a mí los que estáis cansados y agobiados, y yo os aliviaré"</div>
                         <div className="referencia">Mateo 11:28-30</div>
